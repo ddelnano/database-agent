de33568cfb4a366f3951687df7eb3f2969d62de0
Jason McCallister <jason@mccallister.dev>
2025-03-19 14:49:21 -0400

    only postgres ftm

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"dagger/database-agent/internal/dagger"
-	"errors"
-	"fmt"
 )
 
 type ConnectionType string
@@ -14,8 +12,27 @@ const (
 	POSTGRES ConnectionType = "postgres"
 )
 
-var (
-	Prompt = `You are an expert database administrator. You have been given
+type DatabaseAgent struct {
+	// +private
+	//ConnectionType ConnectionType
+	// +private
+	Connection *dagger.Secret
+}
+
+func New(connection *dagger.Secret) *DatabaseAgent {
+	return &DatabaseAgent{
+		//ConnectionType: connectionType,
+		Connection: connection,
+	}
+}
+
+func (m *DatabaseAgent) Ask(ctx context.Context, question string) (*dagger.Container, error) {
+	ws := dag.PostgresWorkspace(m.Connection)
+
+	return dag.Llm().
+		SetPostgresWorkspace("workspace", ws).
+		WithPromptVar("question", question).
+		WithPrompt(`You are an expert database administrator. You have been given
 a workspace with the ability to connect to a database and run SQL queries and you have access to the following tools:
 
 - list-tables
@@ -30,37 +47,7 @@ $question
 </question>
 
 Always show the SQL query you used to get the result.
-DO NOT STOP UNTIL YOU HAVE ANSWERED THE QUESTION COMPLETELY.`
-)
+DO NOT STOP UNTIL YOU HAVE ANSWERED THE QUESTION COMPLETELY.`).
+		Container(), nil
 
-type DatabaseAgent struct {
-	// +private
-	ConnectionType ConnectionType
-	// +private
-	// +default="env:PG_URL"
-	Connection *dagger.Secret
-}
-
-func New(connectionType ConnectionType, connection *dagger.Secret) *DatabaseAgent {
-	return &DatabaseAgent{
-		ConnectionType: connectionType,
-		Connection:     connection,
-	}
-}
-
-func (m *DatabaseAgent) Run(ctx context.Context, question string) (*dagger.Container, error) {
-	switch m.ConnectionType {
-	case POSTGRES:
-		ws := dag.PostgresWorkspace(m.Connection)
-
-		return dag.Llm().
-			SetPostgresWorkspace("workspace", ws).
-			WithPromptVar("question", question).
-			WithPrompt(Prompt).
-			Container(), nil
-	case MYSQL:
-		return nil, fmt.Errorf("mysql is not implemented")
-	}
-
-	return nil, errors.New("unknown connection type")
 }
