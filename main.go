848fa4d3627b8e50104bec7c7800191dc3f0643e
Jason McCallister <jason@mccallister.dev>
2025-03-24 21:54:21 -0400

    cleaning up
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- main.go
+++ main.go
@@ -9,23 +9,23 @@ type DatabaseAgent struct {
 	Connection *dagger.Secret // +private
 }
 
-func New(connection *dagger.Secret) *DatabaseAgent {
-	return &DatabaseAgent{Connection: connection}
-}
+func New(connection *dagger.Secret) *DatabaseAgent { return &DatabaseAgent{Connection: connection} }
 
 func (m *DatabaseAgent) Ask(ctx context.Context, question string) error {
-	_, err := dag.Llm().
-		WithDatabaseWorkspace(dag.DatabaseWorkspace(m.Connection)).
+	_, err := dag.LLM().
+		WithSQL(
+			dag.SQL(m.Connection),
+		).
 		WithPromptVar("question", question).
 		WithPrompt(`You are an expert database administrator. You have been given
-a workspace with the ability to connect to a database and run SQL queries and you have access to the following tools:
+a SQL module with the ability to connect to a database and run SQL queries and you have access to the following tools:
 
 - list-tables
 - list-columns
 - list-column-details
 - run-query
 
-Use ONLY the tools provided in the workspace to answer the question.
+Use the tools provided by the SQL module to answer the question.
 
 <question>
 $question
@@ -33,9 +33,6 @@ $question
 
 Always show the SQL query you used to get the result.
 DO NOT STOP UNTIL YOU HAVE ANSWERED THE QUESTION COMPLETELY.`).LastReply(ctx)
-	if err != nil {
-		return err
-	}
 
-	return nil
+	return err
 }
