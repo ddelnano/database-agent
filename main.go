b924842dac8d8acce50b2da78bec0dd9dcac18fe
Jason McCallister <jason@mccallister.dev>
2025-04-10 21:22:18 -0400

    updating the prompt
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- main.go
+++ main.go
@@ -16,16 +16,13 @@ func (m *DatabaseAgent) Ask(
 	question string,
 ) error {
 	env := dag.Env().
-		WithSQLInput("sql", dag.SQL(dbURL), "The SQL module to use to ask questions").
-		WithStringInput("question", question, "The question being asked about the database")
+		WithSQLInput("sql", dag.SQL(dbURL), "The SQL module to use to inspect the database").
+		WithStringInput("question", question, "The question about the database being asked")
 
-	_, err := dag.LLM().WithEnv(env).WithPrompt(`You are an expert database administrator. You have been given
+	_, err := dag.LLM().WithEnv(env).
+		WithPrompt(`You are an expert database administrator. You have been given
 a SQL module that already has tools with credentials and the ability to connect to the database to run SQL queries.
-
-$question
-
 Always show the SQL query you used to get the result.
 DO NOT STOP UNTIL YOU HAVE ANSWERED THE QUESTION COMPLETELY.`).LastReply(ctx)
-
 	return err
 }
