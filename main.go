d9af609c6a37e6c5f0c67e939229ec812b0931c7
Jason McCallister <jason@mccallister.dev>
2025-04-16 11:26:38 -0400

    update dagger, improve prompt
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- main.go
+++ main.go
@@ -14,15 +14,19 @@ func (m *DatabaseAgent) Ask(
 	dbURL *dagger.Secret,
 	// The question to ask the database agent
 	question string,
-) error {
+) (string, error) {
 	env := dag.Env().
-		WithSQLInput("sql", dag.SQL(dbURL), "The SQL module to use to inspect the database").
-		WithStringInput("question", question, "The question about the database being asked")
+		WithStringInput("question", question, "The question about the database being asked").
+		WithSQLInput("sql", dag.SQL(dbURL), "The SQL module to use to inspect the database")
 
-	_, err := dag.LLM().WithEnv(env).
+	return dag.LLM().
+		WithEnv(env).
 		WithPrompt(`You are an expert database administrator. You have been given
 a SQL module that already has tools with credentials and the ability to connect to the database to run SQL queries.
 Always show the SQL query you used to get the result.
-DO NOT STOP UNTIL YOU HAVE ANSWERED THE QUESTION COMPLETELY.`).LastReply(ctx)
-	return err
+
+The question is: $question
+
+DO NOT STOP UNTIL YOU HAVE ANSWERED THE QUESTION COMPLETELY.`).
+		LastReply(ctx)
 }
